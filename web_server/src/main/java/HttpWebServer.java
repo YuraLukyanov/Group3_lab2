93761be162c601dbf4341feeb27b2ebc3b6f2c99
Nikolay <Soldier180@gmail.com>
2017-05-25 15:24:30 +0300

    Added logging for HttpWebServer

--- web_server/src/main/java/HttpWebServer.java
+++ web_server/src/main/java/HttpWebServer.java
@@ -1,6 +1,8 @@
 
 import controller.ClientSession;
 import controller.Container;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.io.IOException;
@@ -11,11 +13,13 @@ import java.net.Socket;
  * Created by Nikolion on 11.04.2017.
  */
 public class HttpWebServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpWebServer.class);
     public static void main(String[] args) {
         int port = 9999;
         String fileName = "test_beans.xml";
         //String fileName = "beans.xml";
         if (!Container.getInstance().init(fileName)) {
+            LOGGER.error("Server not started. Can't init container");
             System.out.println("Server not started. Can't init container");
             System.exit(-1);
         }
@@ -24,8 +28,10 @@ public class HttpWebServer {
             serverSocket = new ServerSocket(port);
             System.out.println("Server started on port: "
                     + serverSocket.getLocalPort() + "\n");
+            LOGGER.info("Server started on port: "+ serverSocket.getLocalPort() + "\n");
         } catch (IOException e) {
             System.out.println("Port " + port + " is blocked.");
+            LOGGER.error("Port " + port + " is blocked.");
             System.exit(-1);
         }
 
@@ -36,11 +42,10 @@ public class HttpWebServer {
                 session.start();
                 Thread.sleep(100);//for more stability
             } catch (IOException e) {
-                System.out.println("Failed to establish connection.");
-                System.out.println(e.getMessage());
+                LOGGER.error("Failed to establish connection",e);
                 System.exit(-1);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                LOGGER.error("Thread error",e);
             }
         }
 
